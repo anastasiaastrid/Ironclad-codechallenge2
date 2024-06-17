fc5bef3a5e444fab5767e1dd82e03961a3e1ff11
astridann <hasan.astrid@gmail.com>
2024-06-17 19:46:31 +0700

    a

--- src/views/products-view/signatureedition.tsx
+++ src/views/products-view/signatureedition.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
@@ -31,7 +30,7 @@ function SignatureEdition() {
       <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
         {data?.map((datamap) => (
           // @ts-ignore
-          <div key={datamap.sys.id} className="bg-zinc-900 font-Anek_Devanagari pt-5 pb-8 px-4 sm:px-6 lg:px-8">
+          <div key={datamap.sys.id} className="bg-zinc-900 text-white font-Anek_Devanagari pt-5 pb-8 px-4 sm:px-6 lg:px-8">
             <div className="px-4 sm:px-0">
               <p className="text-3xl lg:text-5xl text-white font-ZenDots text-center">
                 {datamap.fields.companyOverviewTitle}
@@ -41,81 +40,34 @@ function SignatureEdition() {
               </p>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4 sm:px-0">
-              <div className="text-gray-200">
-                <div className="relative">
-                  <Image
-                    className="rounded-lg"
-                    alt="Product Image 1"
-                    // @ts-ignore
-                    src={`https:${datamap.fields.imageProductSection1?.fields.file.url}`}
-                    width={500}
-                    height={500}
-                    loading="lazy"
-                  />
+              {[1, 2, 3].map((index) => (
+                <div key={index} className="text-gray-200">
+                  <div className="relative h-80">
+                    <Image
+                      className="rounded-lg"
+                      alt={`Product Image ${index}`}
+                      //  @ts-ignore
+                      src={`https:${datamap.fields[`imageProductSection${index}`]?.fields.file.url}`}
+                      width={500}
+                      height={500}
+                      loading="lazy"
+                    />
+                  </div>
+                  <p className="text-xl text-gray-200 font-ZenDots pt-3 text-center lg:text-left">
+                   {/* @ts-ignore */}
+                    {datamap.fields[`titleProductSection${index}`]}
+                  </p>
+                  <div className="rounded-lg text-justify px-4 sm:px-0">
+                    {/* @ts-ignore */}
+                    <RichText document={datamap.fields[`productSection${index}`] || ""} />
+                  </div>
+                  <div className="pt-2 px-4 sm:px-0 text-center">
+                    <Link href="/product/">
+                      <button className="px-4 py-2 font-bold text-black rounded-full bg-white">Shop Now</button>
+                    </Link>
+                  </div>
                 </div>
-                <p className="text-xl text-gray-200 font-ZenDots pt-3 text-center lg:text-left">
-                  {/* @ts-ignore */}
-                  {datamap.fields.titleProductSection1}
-                </p>
-                <div className="rounded-lg text-justify px-4 sm:px-0">
-                  <RichText document={datamap.fields.productSection1 || ""} />
-                </div>
-                <div className="pt-2 px-4 sm:px-0 text-center">
-                  <Link href="/product/">
-                    <button className="px-4 py-2 font-bold text-black rounded-full bg-white">Shop Now</button>
-                  </Link>
-                </div>
-              </div>
-              <div className="text-gray-200">
-                <div className="relative">
-                  <Image
-                    className="rounded-lg"
-                    alt="Product Image 2"
-                    // @ts-ignore
-                    src={`https:${datamap.fields.imageProductSection2?.fields.file.url}`}
-                    width={500}
-                    height={500}
-                    loading="lazy"
-                  />
-                </div>
-                <p className="text-xl text-gray-200 font-ZenDots pt-3 text-center lg:text-left">
-                  {/* @ts-ignore */}
-                  {datamap.fields.titleProductSection2}
-                </p>
-                <div className="rounded-lg text-justify px-4 sm:px-0">
-                  <RichText document={datamap.fields.productSection2 || ""} />
-                </div>
-                <div className="pt-2 px-4 sm:px-0 text-center">
-                  <Link href="/product/">
-                    <button className="px-4 py-2 font-bold text-black rounded-full bg-white">Shop Now</button>
-                  </Link>
-                </div>
-              </div>
-              <div className="text-gray-200">
-                <div className="relative">
-                  <Image
-                    className="rounded-lg"
-                    alt="Product Image 3"
-                    // @ts-ignore
-                    src={`https:${datamap.fields.imageProductSection3?.fields.file.url}`}
-                    width={500}
-                    height={500}
-                    loading="lazy"
-                  />
-                </div>
-                <p className="text-xl text-gray-200 font-ZenDots pt-3 text-center lg:text-left">
-                  {/* @ts-ignore */}
-                  {datamap.fields.titleProductSection3}
-                </p>
-                <div className="rounded-lg text-justify px-4 sm:px-0">
-                  <RichText document={datamap.fields.productSection3 || ""} />
-                </div>
-                <div className="pt-2 px-4 sm:px-0 text-center">
-                  <Link href="/product/">
-                    <button className="px-4 py-2 font-bold text-black rounded-full bg-white">Shop Now</button>
-                  </Link>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         ))}
