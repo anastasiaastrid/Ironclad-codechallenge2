1435523ce7c2ee0c0b9125668ab0c37d7773be5c
astridann <hasan.astrid@gmail.com>
2024-06-18 17:52:08 +0700

    pushed

--- src/views/products-view/signatureedition.tsx
+++ src/views/products-view/signatureedition.tsx
@@ -15,9 +15,9 @@ function SignatureEdition() {
     async function fetchData() {
       try {
         const response = await client.getEntries<TypeBlogFields>();
-        //@ts-ignore
+        // @ts-ignore
         setData(response?.items || []);
-        setShowContent(true); 
+        setShowContent(true); // Show content after fetching data
       } catch (err) {
         console.log(err);
       }
@@ -26,7 +26,7 @@ function SignatureEdition() {
   }, []);
 
   return (
-    <div className="bg-white min-h-full">
+    <div className="bg-white min-h-full w-full">
       <Head>
         <title>Ironclad Watch Signature Edition Products</title>
         <link
@@ -44,42 +44,37 @@ function SignatureEdition() {
           crossOrigin="anonymous"
         />
       </Head>
-
       {showContent && (
-        <div className="max-w-full my-auto py-5 px-4 sm:px-6 lg:px-8">
-          {data.map((datamap) => (
+        <div className="max-w-full max-h-full my-auto mx-auto py-5 px-4 sm:px-6 ">
+          {data?.map((datamap) => (
             <div
-              //@ts-ignore
+              // @ts-ignore
               key={datamap.sys.id}
-              className="bg-zinc-900 text-white font-Anek_Devanagari py-12 px-4 sm:px-6 lg:px-8 mb-8 rounded-lg"
+              className="bg-zinc-900 text-white font-Anek_Devanagari pt-12 pb-8 px-4 sm:px-6 lg:px-8 mb-8"
             >
               <div className="px-4 sm:px-0">
                 <p className="text-3xl lg:text-5xl text-white font-ZenDots text-center mb-6">
                   {datamap.fields.companyOverviewTitle}
                 </p>
-                <p className="text-white font-BakbakOne tracking-widest text-center mb-8">
+                <p className="text-white font-BakbakOne tracking-widest pb-4 text-center mb-8">
                   {datamap.fields.productTitle}
                 </p>
               </div>
-
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4 sm:px-0">
-                {/* Product Section 1 */}
-                <div className="text-gray-200 flex flex-col items-center justify-center">
-                  <div className="relative sm:h-96 md:h-120 lg:h-144 h-[302px] w-[434px] lg:w-full">
-                    <div className="absolute inset-0 flex justify-center items-center">
-                      <Image
-                        className="rounded-lg"
-                        alt={`Product Image 1`}
-                        //@ts-ignore
-                        src={`https:${datamap.fields.imageProductSection1?.fields.file.url}`}
-                        layout="fill"
-                        objectFit="cover"
-                        loading="lazy"
-                      />
-                    </div>
+                <div className="text-gray-200">
+                  <div className="relative h-80 sm:h-64 md:h-80 lg:h-96">
+                    <Image
+                      className="rounded-lg"
+                      alt={`Product Image 1`}
+                      // @ts-ignore
+                      src={`https:${datamap.fields.imageProductSection1?.fields.file.url}`}
+                      layout="fill"
+                      objectFit="cover"
+                      loading="lazy"
+                    />
                   </div>
                   <p className="text-xl text-gray-200 font-ZenDots pt-4 text-center lg:pt-8 mb-4">
-                    {/* @ts-ignore */}
+                    {/** @ts-ignore */}
                     {datamap.fields.titleProductSection1}
                   </p>
                   <div className="rounded-lg text-justify px-4 sm:px-0 mb-4">
@@ -92,23 +87,20 @@ function SignatureEdition() {
                   </div>
                 </div>
 
-                {/* Product Section 2 */}
-                <div className="text-gray-200 flex flex-col items-center justify-center">
-                  <div className="relative sm:h-96 md:h-120 lg:h-144 h-[302px] w-[434px] lg:w-full">
-                    <div className="absolute inset-0 flex justify-center items-center">
-                      <Image
-                        className="rounded-lg"
-                        alt={`Product Image 2`}
-                        //@ts-ignore
-                        src={`https:${datamap.fields.imageProductSection2?.fields.file.url}`}
-                        layout="fill"
-                        objectFit="cover"
-                        loading="lazy"
-                      />
-                    </div>
+                <div className="text-gray-200">
+                  <div className="relative h-80 sm:h-64 md:h-80 lg:h-96">
+                    <Image
+                      className="rounded-lg"
+                      alt={`Product Image 2`}
+                      // @ts-ignore
+                      src={`https:${datamap.fields.imageProductSection2?.fields.file.url}`}
+                      layout="fill"
+                      objectFit="cover"
+                      loading="lazy"
+                    />
                   </div>
                   <p className="text-xl text-gray-200 font-ZenDots pt-4 text-center lg:pt-8 mb-4">
-                    {/* @ts-ignore */}
+                    {/** @ts-ignore */}
                     {datamap.fields.titleProductSection2}
                   </p>
                   <div className="rounded-lg text-justify px-4 sm:px-0 mb-4">
@@ -121,23 +113,20 @@ function SignatureEdition() {
                   </div>
                 </div>
 
-                {/* Product Section 3 */}
-                <div className="text-gray-200 flex flex-col items-center justify-center">
-                  <div className="relative sm:h-96 md:h-120 lg:h-144 h-[302px] w-[434px] lg:w-full">
-                    <div className="absolute inset-0 flex justify-center items-center">
-                      <Image
-                        className="rounded-lg"
-                        alt={`Product Image 3`}
-                        //@ts-ignore
-                        src={`https:${datamap.fields.imageProductSection3?.fields.file.url}`}
-                        layout="fill"
-                        objectFit="cover"
-                        loading="lazy"
-                      />
-                    </div>
+                <div className="text-gray-200">
+                  <div className="relative h-80 sm:h-64 md:h-80 lg:h-96">
+                    <Image
+                      className="rounded-lg"
+                      alt={`Product Image 3`}
+                      // @ts-ignore
+                      src={`https:${datamap.fields.imageProductSection3?.fields.file.url}`}
+                      layout="fill"
+                      objectFit="cover"
+                      loading="lazy"
+                    />
                   </div>
                   <p className="text-xl text-gray-200 font-ZenDots pt-4 text-center lg:pt-8 mb-4">
-                    {/* @ts-ignore */}
+                    {/** @ts-ignore */}
                     {datamap.fields.titleProductSection3}
                   </p>
                   <div className="rounded-lg text-justify px-4 sm:px-0 mb-4">
@@ -154,6 +143,7 @@ function SignatureEdition() {
           ))}
         </div>
       )}
+
     </div>
   );
 }
