a1a8babe3ef7cd6a786daecdbc9a38d88a890622
astridann <hasan.astrid@gmail.com>
2024-06-20 09:18:57 +0700

    text

--- src/views/home-view/home-section/HeroSection.tsx
+++ src/views/home-view/home-section/HeroSection.tsx
@@ -48,10 +48,10 @@ function HeroSection() {
 
             <div className=" absolute inset-0 flex flex-col justify-center px-10 py-10  lg:p-28 sm:p-full sm:w-full bg-black bg-opacity-50 text-white">
               <p className=" text-2xl lg:text-7xl font-ZenDots sm:p-full sm:w-full">{datamap.fields.title}</p>
-              <p className=" pt-4 lg:pt-6 text-base lg:text-2xl sm:p-full sm:w-full tracking-wide font-BakbakOne">
+              <p className=" lg:pt-2 lg:pt-6 text-base lg:text-2xl sm:p-full sm:w-full tracking-wide font-BakbakOne">
                 {datamap.fields.summary}
               </p>
-              <div className="font-Anek_Devanagari text-sm pr-10 pt-4 lg:pt-6 lg:text-lg sm:p-full sm:w-full">
+              <div className="font-Anek_Devanagari text-sm pr-10 pt-2 lg:pt-6 lg:text-lg sm:p-full sm:w-full">
                 <RichText document={datamap.fields.details} />
               </div>
               <div className="w-1/2 pt-4 lg:pt-6">
